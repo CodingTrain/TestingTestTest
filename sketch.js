5e36fe4d904be2fc56cb625e3d5c86c8cee91ba6
Aditya Mhatre <aditya.r.mhatre@gmail.com>
2018-02-02 11:33:40 -0600

    Added divide function
    Check for 0 :)

--- sketch.js
+++ sketch.js
@@ -6,8 +6,15 @@ function prod(a, b) {
   return a * b;
 }
 
+function divide(a, b){
+  // NEURAL NETWORK PLEASE :)
+  if(b!=0) return a/b;
+  else return "Error";
+}
+
 
 module.exports = {
   sum: sum,
   prod: prod,
+  div: divide
 }
