2824e045662058f788a842bfa731464b5558a3a4
BlackOfWorld <BlackOfWorld@users.noreply.github.com>
2018-02-02 18:30:47 +0100

    👌

--- sketch.test.js
+++ sketch.test.js
@@ -4,8 +4,8 @@ const sum = require('./sketch');
 //   expect(sum(1, 2)).toBe(3);
 // });
 
-test('adds 1 + 2 to equal 3', sumTest);
+test('adds 1 + 5 to equal 999', sumTest);
 
 function sumTest() {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 5)).toBe(999);
 }
