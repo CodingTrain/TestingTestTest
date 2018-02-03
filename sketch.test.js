46902e382ade20eabbe0655aff099f810a7e109e
nobobo1234 <noahvanboven@gmail.com>
2018-02-03 16:38:15 +0100

    Adding digital root

--- sketch.test.js
+++ sketch.test.js
@@ -1,4 +1,4 @@
-const { sum, prod } = require('./sketch');
+const { sum, prod, digital_root } = require('./sketch');
 
 // test('adds 1 + 2 to equal 3', () => {
 //   expect(sum(1, 2)).toBe(3);
@@ -13,4 +13,8 @@ function sumTest() {
 
 test('prod calculates 2 * 10 = 20', () => {
   expect(prod(2, 10)).toBe(20);
+})
+
+test('digital root of 265 should equal 4', () => {
+  expect(digital_root(265)).toBe(4);
 })
