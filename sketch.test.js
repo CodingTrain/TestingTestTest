69448f2ac9a67f238faf3d788264a9b0f1294fd6
the349 <josephmalone25@gmail.com>
2018-02-11 22:59:08 -0500

    Update sketch.test.js

--- sketch.test.js
+++ sketch.test.js
@@ -1,4 +1,4 @@
-const { sum, prod, digital_root } = require('./sketch');
+const { sum, prod, digital_root, sum42 } = require('./sketch');
 
 // test('adds 1 + 2 to equal 3', () => {
 //   expect(sum(1, 2)).toBe(3);
